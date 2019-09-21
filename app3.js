1c1ff50a066871951f9451065289cc13a82b00e1
mgu0 <12101668+mguomanila@users.noreply.github.com>
2019-09-22 02:04:11 +0800

    modify

--- app3.js
+++ app3.js
@@ -4,6 +4,7 @@ const request = require('request')
 const fs = require('fs')
 const path = require('path')
 const {EventEmitter} = require('events') 
+// const {parsePdf} = require('test_pdf2')
 
 const app = express()
 const {numbers,modifyFile} = require('./numbers1')
@@ -12,8 +13,8 @@ const event = new EventEmitter()
 const URL = 'https://ekrs.ms.gov.pl/web/wyszukiwarka-krs/strona-glowna/'
 const timeout = 1e3
 const local_url = 'http://localhost:6060/get'
-const OUT_DIR1 = process.env.OUT_DIR || `downloads3`
-const OUT_DIR2 = process.env.OUT_DIR || `downloads4`
+const OUT_DIR1 = process.env.OUT_DIR || `Rejestr Przedsiębiorców`
+const OUT_DIR2 = process.env.OUT_DIR || `Rejestr Stowarzyszeń`
 const DOWNLOAD_PATH1 = path.resolve(__dirname, OUT_DIR1)
 const DOWNLOAD_PATH2 = path.resolve(__dirname, OUT_DIR2)
 
@@ -33,12 +34,16 @@ function mkdirSync(dirPath) {
     }
 }
   
-async function findPDFAndDownload(page){
+async function findPDFAndDownload1(page){
     const timeout = 1e3
+    await new Promise( resolve => {
+        setTimeout( () => resolve(), timeout*6 )
+    })
     await new Promise (resolve => {
         document.querySelector('iframe')
         .contentWindow.document.body
-        .querySelector('label.input_name b')
+        // .querySelector('label.input_name b')
+        .querySelector('#rejestrPrzedsiebiorcy')
         .click()
         resolve()
     })
@@ -46,7 +51,8 @@ async function findPDFAndDownload(page){
         document.querySelector('iframe')
         .contentWindow.document.body
         .querySelector('input#krs')
-        .value = '0000066488'
+        // .value = '0000586006'
+        .value = page.number
         resolve()
     })
 
@@ -110,7 +116,7 @@ async function findPDFAndDownload(page){
     })
 
     await new Promise( resolve => {
-        setTimeout( () => resolve(), timeout*4 )
+        setTimeout( () => resolve(), timeout*6 )
     })
     
     await new Promise( resolve => {
@@ -137,7 +143,117 @@ async function findPDFAndDownload(page){
     })
 }
 
-async function crawl(browser, page){
+async function findPDFAndDownload2(page){
+    const timeout = 1e3
+    await new Promise( resolve => {
+        setTimeout( () => resolve(), timeout*4 )
+    })
+    await new Promise (resolve => {
+        document.querySelector('iframe')
+        .contentWindow.document.body
+        // .querySelector('label.input_name b')
+        .querySelector('#rejestrStowarzyszenia')
+        .click()
+        resolve()
+    })
+    await new Promise( resolve => {
+        document.querySelector('iframe')
+        .contentWindow.document.body
+        .querySelector('input#krs')
+        // .value = '0000586006'
+        // .value = '0000066488'
+        .value = page.number
+        resolve()
+    })
+
+    let captcha
+    let scripts
+    
+    captcha = document.createElement('iframe', {
+        title:"recaptcha challenge",
+        src: "https://www.google.com/recaptcha/api2/bframe?hl=en&amp;v=v1566858990656&amp;k=6LepijEUAAAAAIx-XxXOV_b0puYkdP7Niz5gJsC1&amp;cb=a94c83j0nwef",
+        name: "c-ohkmxkft270q",
+        frameborder: "0",
+        scrolling: "no",
+        sandbox: "allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox",
+        style: "width: 100%; height: 100%;"
+    })
+
+    await new Promise( resolve => {
+        setTimeout( () => resolve(), timeout*4 )
+    })
+        
+    await new Promise( resolve => {
+        scripts = document.querySelector('iframe')
+            .contentWindow.document.body
+            .querySelectorAll('iframe')
+        resolve()
+    })
+
+    const elem1 = scripts[0]
+    const elem2 = scripts[1]
+    // const parent1 = elem1.parentNode
+    // const parent2 = elem2.parentNode
+    // parent1.removeChild(elem1)
+    // parent1.appendChild(captcha)
+    // parent2.removeChild(elem2)
+    const parent = scripts[0].parentNode
+    parent.removeChild(scripts[0])
+    parent.appendChild(captcha)
+    scripts[1].parentNode.removeChild(scripts[1])
+    
+    await new Promise( resolve => {
+        document.querySelector('iframe')
+        .contentWindow.document.body
+        .querySelector('#g-recaptcha-response')
+        .innerHTML = page.token
+        resolve()
+    })
+    await new Promise( resolve => {
+        document.querySelector('iframe')
+        .contentWindow.document.body
+        .querySelector('#captchaToken')
+        .value = page.token
+        resolve()
+    })
+    await new Promise( resolve => {
+        const button = document.querySelector('iframe')
+        .contentWindow.document.body
+        .querySelector('#szukaj')
+        button.scrollIntoView()
+        button.click()
+        resolve()
+    })
+
+    await new Promise( resolve => {
+        setTimeout( () => resolve(), timeout*6 )
+    })
+    
+    await new Promise( resolve => {
+        const goto = document.querySelector('iframe')
+            .contentWindow.document.body
+            .querySelector('#podmiotyGrid a[href]')
+        goto.scrollIntoView()
+        goto.click()
+        resolve()
+    })
+
+    await new Promise( resolve => {
+        setTimeout( () => resolve(), timeout*4 )
+    })
+
+    await new Promise( resolve => {
+        const pdf = document.querySelector('iframe')
+            .contentWindow.document.body
+            .querySelector('#pobierzWydrukPelny')
+        pdf.target = ''
+        pdf.scrollIntoView()
+        pdf.click()
+        setTimeout( () => resolve(), timeout*5 )
+    })
+}
+
+async function crawl1(browser, page){
     console.log(`Loading: ${page.url}`)
     console.log(`using #${page.number}`)
     console.log('token: ', page.token)
@@ -149,7 +265,28 @@ async function crawl(browser, page){
     })
     await newPage.goto(page.url, {waitUntil: 'networkidle2'})
     try{
-        await newPage.evaluate(findPDFAndDownload, page)
+        await newPage.evaluate(findPDFAndDownload1, page)
+    }catch(e){
+        console.log(e)
+        newPage.close()
+    }
+    await newPage.close()
+    // event.emit('finished')
+}
+
+async function crawl2(browser, page){
+    console.log(`Loading: ${page.url}`)
+    console.log(`using #${page.number}`)
+    console.log('token: ', page.token)
+    const newPage = await browser.newPage()
+    const client = await newPage.target().createCDPSession()
+    await client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: DOWNLOAD_PATH2
+    })
+    await newPage.goto(page.url, {waitUntil: 'networkidle2'})
+    try{
+        await newPage.evaluate(findPDFAndDownload2, page)
     }catch(e){
         console.log(e)
         newPage.close()
@@ -163,20 +300,32 @@ function modifyNumbers(n){
 }
 
 (async() => {
+
     event.on('finished', e => {
         if(e) console.log('an error occured',e)
         browser.close()
     })
+    
     event.on('numbers_change', n => {
         modifyNumbers(n)
     })
+    
     event.on('timeout', ()=>{
         clearInterval(interval_id)
     })
+
+    event.on('clearInterval', ()=>{
+        clearInterval(intervalId)
+    })
+    
     mkdirSync(OUT_DIR1) // create output dir if it doesn't exist
+    mkdirSync(OUT_DIR2)
     const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--enable-logging', '--disable-gpu ', '--disable-software-rasterizer'],
+        // headless: false,
+        args: ['--enable-logging', '--disable-gpu ', '--disable-software-rasterizer', 
+        //'--disable-web-security', 
+        '--user-data-dir=./chrome'
+    ],
         dumpio: true
     })
     
@@ -185,24 +334,51 @@ function modifyNumbers(n){
     let interval_id
     let counter = 0
 
-    event.on('tokens', async(token)=>{
+    event.on('tokens1', async(token,n)=>{
         await Promise.all([
             root.token = token,
-            root.number = numbers.pop(),
+            root.number = n,
             event.emit('numbers_change', numbers)
             // modifyNumbers(numbers)
         ])
-        await crawl(browser, root)
+        try{
+            await crawl1(browser, root)
+        }catch(e){
+            console.log(e)
+            event.emit('clearInterval')
+            event.emit('finished')
+        }  
     })
-
-    setInterval( () => {
+    event.on('tokens2', async(token,n)=>{
+        await Promise.all([
+            root.token = token,
+            root.number = n
+            // event.emit('numbers_change', numbers)
+            // modifyNumbers(numbers)
+        ])
+        try{
+            await crawl2(browser, root)
+        }catch(e){
+            console.log(e)
+            event.emit('clearInterval')
+            event.emit('finished')
+        }  
+    })
+    let count=0
+    let number
+    var intervalId = setInterval( () => {
         request.get({url: local_url}, (e,r,b)=>{
             token = JSON.parse(b)['token']
-            if(!!token) {
+            if(!!token && !count) {
                 // tokens.push(token)
-                event.emit('tokens',token)
+                number = numbers.pop()
+                event.emit('tokens1',token, number)
+                count++
+            }else if(!!token && !!count){
+                event.emit('tokens2',token, number)
+                count=0
             }
         })
-    },timeout)
+    },timeout*10)
 
 })()
